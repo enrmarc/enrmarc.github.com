534a52e36cf29a5ebee5684afcfb23df65c0629a
enrmarc <enrmarc@gmail.com>
2013-03-30 02:09:26 +0100

    repos.json updated

--- assets/js/myrepo.js
+++ assets/js/myrepo.js
@@ -1,15 +1,30 @@
-$(function() {
+(function($, undefined) {
+   $(function() {
+     $.getJSON('http://enrmarc.github.com/data/repos.json?callback=?', f);
+   });
 
-   setTimeout(function() {
-      $('#num-repos').text(10);
-   }, 1200);
+})(jQuery);
 
-
-
-
-   /*$.getJSON("https://api.github.com/users/enrmarc/repos?callback=?", function (result) {
-      $(function() {
+function f(result) {
+   $(function() {
+      setTimeout(function() {
          $('#num-repos').text(result.data.length);
+      }, 1000);
+
+      $.each(result.data, function(i, repo) {
+         if (!repo.fork) {
+           add(repo);
+         }
       });
-   });*/
-});
+   });
+
+
+   function add(repo) {
+      var $item = $("<li>").addClass("repo span3 " + (repo.language || '').toLowerCase());
+      var $link = $("<a>").attr("href", repo.html_url).appendTo($item);
+      $link.append($("<h2>").text(repo.name));      
+      $link.append($("<h3>").text(repo.language || ''));
+      $link.append($("<p>").text(repo.description));
+      $item.appendTo("#repos");
+   }
+}
