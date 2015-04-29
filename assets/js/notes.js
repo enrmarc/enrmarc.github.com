03c8cfe8edf3e10ae3bdb1be9a2884b3ffa08edd
enrmarc <enrmarc@gmail.com>
2015-04-30 00:31:44 +0200

    Add note: Enviad mas paramedicos

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
       'Noto un desespero enfermizo en tu risa',
       'Esto no debería presentar ningún problema significativo',
       'Sick Boy: Para ser vegetariano, Rents, eres un tirador de lo más sanguinario',
+      'Enviad más paramédicos',
    ];
    document
       .getElementById('notas')
